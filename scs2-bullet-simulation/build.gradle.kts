9ebe62369531ba88b66ec927786d2fe1891f8d67
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-29 18:47:44 -0500

    :arrow_up: ihmc-yovariables 0.13.3 :arrow_up: euclid 0.22.2

--- scs2-bullet-simulation/build.gradle.kts
+++ scs2-bullet-simulation/build.gradle.kts
@@ -17,9 +17,9 @@ mainDependencies {
    api("us.ihmc:scs2-definition:source")
    api("us.ihmc:scs2-shared-memory:source")
    api("us.ihmc:scs2-session:source")
-   api("us.ihmc:euclid-frame-shape:0.22.0")
+   api("us.ihmc:euclid-frame-shape:0.22.2")
    api("us.ihmc:ihmc-messager:0.2.0")
-   api("us.ihmc:ihmc-yovariables:0.13.2")
+   api("us.ihmc:ihmc-yovariables:0.13.3")
    api("us.ihmc:mecano-yovariables:17-0.19.0")
 
    apiBytedecoNatives("javacpp")
--- scs2-symbolic/build.gradle.kts
+++ scs2-symbolic/build.gradle.kts
@@ -14,9 +14,9 @@ mainDependencies {
    api("us.ihmc:scs2-definition:source")
    api("us.ihmc:scs2-shared-memory:source")
 
-   api("us.ihmc:euclid:0.22.0")
-   api("us.ihmc:euclid-frame:0.22.0")
-   api("us.ihmc:ihmc-yovariables:0.13.2")
+   api("us.ihmc:euclid:0.22.2")
+   api("us.ihmc:euclid-frame:0.22.2")
+   api("us.ihmc:ihmc-yovariables:0.13.3")
 }
 
 testDependencies {
