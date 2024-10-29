0d1b9faa8ce45e3782ef0d4e0911a09c227154e8
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2024-10-29 18:56:30 -0500

    Refactor SCS2YoGraphicHolder and SCS2DefinitionMIssingTools into SCS2 (#192)
    Moved SCS2YoGraphicHolder and SCS2DefinitionMissingTools into SCS2, they
    live in scs2-definition right next to the graphics they represent
    
    ---------
    
    Co-authored-by: rgriffin@ihmc.org <rgriffin@ihmc.us>

--- scs2-definition/build.gradle.kts
+++ scs2-definition/build.gradle.kts
@@ -11,12 +11,12 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:euclid:0.21.0")
-   api("us.ihmc:euclid-shape:0.21.0")
-   api("us.ihmc:euclid-frame:0.21.0")
-   api("us.ihmc:ihmc-commons:0.32.0")
-   api("us.ihmc:ihmc-yovariables:0.12.2")
-   api("us.ihmc:mecano:17-0.18.1")
+   api("us.ihmc:euclid:0.22.2")
+   api("us.ihmc:euclid-shape:0.22.2")
+   api("us.ihmc:euclid-frame:0.22.2")
+   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-yovariables:0.13.3")
+   api("us.ihmc:mecano:17-0.19.0")
 }
 
 testDependencies {
