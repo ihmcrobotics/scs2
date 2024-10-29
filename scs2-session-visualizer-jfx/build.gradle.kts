6c3e8f8aefc06a4cf248649d5090b558c1cafb4b
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2024-10-29 18:56:30 -0500

    Refactor SCS2YoGraphicHolder and SCS2DefinitionMIssingTools into SCS2 (#192)
    Moved SCS2YoGraphicHolder and SCS2DefinitionMissingTools into SCS2, they
    live in scs2-definition right next to the graphics they represent
    
    ---------
    
    Co-authored-by: rgriffin@ihmc.org <rgriffin@ihmc.us>

--- scs2-session-visualizer-jfx/build.gradle.kts
+++ scs2-session-visualizer-jfx/build.gradle.kts
@@ -27,9 +27,9 @@ mainDependencies {
    api(ihmc.javaFXModule("fxml", javaFXVersion))
    api(ihmc.javaFXModule("swing", javaFXVersion))
 
-   api("us.ihmc:euclid:0.21.0")
-   api("us.ihmc:euclid-shape:0.21.0")
-   api("us.ihmc:euclid-frame:0.21.0")
+   api("us.ihmc:euclid:0.22.2")
+   api("us.ihmc:euclid-shape:0.22.2")
+   api("us.ihmc:euclid-frame:0.22.2")
    api("us.ihmc:ihmc-graphics-description:0.25.1")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:ihmc-javafx-extensions:17-0.2.1")
--- scs2-shared-memory/build.gradle.kts
+++ scs2-shared-memory/build.gradle.kts
@@ -12,9 +12,9 @@ ihmc {
 
 mainDependencies {
    api("us.ihmc:scs2-definition:source")
-   api("us.ihmc:euclid:0.21.0")
-   api("us.ihmc:euclid-frame:0.21.0")
-   api("us.ihmc:ihmc-yovariables:0.12.2")
+   api("us.ihmc:euclid:0.22.2")
+   api("us.ihmc:euclid-frame:0.22.2")
+   api("us.ihmc:ihmc-yovariables:0.13.3")
    api("us.hebi.matlab.mat:mfl-core:0.5.7")
 }
 
