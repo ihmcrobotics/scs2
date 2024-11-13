d4cb937e9024b0527c52078ac0a47bcd2a01b74b
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2024-11-13 12:57:44 -0600

    Feature/gui tools (#195)
    Co-authored-by: James Foster <jfoster@ihmc.org>

--- scs2-session-visualizer-jfx/src/main/java/us/ihmc/scs2/sessionVisualizer/jfx/controllers/menu/YoChartMenuController.java
+++ scs2-session-visualizer-jfx/src/main/java/us/ihmc/scs2/sessionVisualizer/jfx/controllers/menu/YoChartMenuController.java
@@ -61,6 +61,12 @@ public class YoChartMenuController implements VisualizerController
    }
 
    @FXML
+   public void openPieChartWindow()
+   {
+      messager.submitMessage(topics.getOpenWindowRequest(), NewWindowRequest.pieChartWindow(owner));
+   }
+
+   @FXML
    public void loadChartGroup()
    {
       File result = SessionVisualizerIOTools.yoChartConfigurationOpenFileDialog(owner);
