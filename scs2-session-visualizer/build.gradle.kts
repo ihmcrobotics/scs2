5303a1c0df5769784aac231728b21e43e0548aa4
ds58 <30220598+ds58@users.noreply.github.com>
2025-01-23 16:46:51 -0600

    :arrow_down: openjfx 17.0.8 (#197)

--- scs2-session-visualizer/build.gradle.kts
+++ scs2-session-visualizer/build.gradle.kts
@@ -12,7 +12,7 @@ ihmc {
 mainDependencies {
    api("us.ihmc:scs2-definition:source")
 
-   var javaFXVersion = "17.0.9"
+   var javaFXVersion = "17.0.8"
    api(ihmc.javaFXModule("base", javaFXVersion)) // This is for using the property data structure. Not sure if that's the best thing to do.
 }
 
