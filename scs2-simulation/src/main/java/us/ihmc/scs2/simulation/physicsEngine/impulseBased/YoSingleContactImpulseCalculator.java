614828665f2b438f6a32973a3fa08afce1541d74
James Foster <jfoster@ihmc.org>
2025-02-03 08:15:18 -0600

    Switch to ihmc-yovariables YoMatrix; remove SCS2 YoMatrix (#200)

--- scs2-simulation/src/main/java/us/ihmc/scs2/simulation/physicsEngine/impulseBased/YoSingleContactImpulseCalculator.java
+++ scs2-simulation/src/main/java/us/ihmc/scs2/simulation/physicsEngine/impulseBased/YoSingleContactImpulseCalculator.java
@@ -18,7 +18,7 @@ import us.ihmc.mecano.multiBodySystem.iterators.SubtreeStreams;
 import us.ihmc.mecano.yoVariables.spatial.YoFixedFrameSpatialVector;
 import us.ihmc.mecano.yoVariables.spatial.YoFixedFrameTwist;
 import us.ihmc.scs2.simulation.collision.CollisionResult;
-import us.ihmc.scs2.simulation.physicsEngine.YoMatrix;
+import us.ihmc.yoVariables.math.YoMatrix;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoint3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
 import us.ihmc.yoVariables.registry.YoRegistry;
