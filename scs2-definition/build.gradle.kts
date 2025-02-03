2314c4d1cde8bdaff623f9da5706e4cecbf182a9
James Foster <jfoster@ihmc.org>
2025-02-03 08:12:42 -0600

    :arrow_up: ihmc-yovariables 0.13.4 (#201)

--- scs2-definition/build.gradle.kts
+++ scs2-definition/build.gradle.kts
@@ -14,7 +14,7 @@ mainDependencies {
    api("us.ihmc:euclid-shape:0.22.2")
    api("us.ihmc:euclid-frame:0.22.2")
    api("us.ihmc:ihmc-commons:0.34.0")
-   api("us.ihmc:ihmc-yovariables:0.13.3")
+   api("us.ihmc:ihmc-yovariables:0.13.4")
    api("us.ihmc:mecano:17-0.19.0")
 }
 
