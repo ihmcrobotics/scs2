322944604dd6ee1037655918449af52ae91b1453
ds58 <30220598+ds58@users.noreply.github.com>
2025-02-06 11:42:42 -0600

    :arrow_up: euclid 0.22.3 (#207)

--- scs2-definition/build.gradle.kts
+++ scs2-definition/build.gradle.kts
@@ -10,9 +10,9 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:euclid:0.22.2")
-   api("us.ihmc:euclid-shape:0.22.2")
-   api("us.ihmc:euclid-frame:0.22.2")
+   api("us.ihmc:euclid:0.22.3")
+   api("us.ihmc:euclid-shape:0.22.3")
+   api("us.ihmc:euclid-frame:0.22.3")
    api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:ihmc-yovariables:0.13.5")
    api("us.ihmc:mecano:17-0.19.0")
