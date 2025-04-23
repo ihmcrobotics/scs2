6438aa0e2d77847ae97de7879ec43ded4c89de21
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2025-04-23 09:59:44 -0500

    :up_arrow: euclid 0.22.4 (#211)

--- scs2-symbolic/build.gradle.kts
+++ scs2-symbolic/build.gradle.kts
@@ -13,8 +13,8 @@ mainDependencies {
    api("us.ihmc:scs2-definition:source")
    api("us.ihmc:scs2-shared-memory:source")
 
-   api("us.ihmc:euclid:0.22.3")
-   api("us.ihmc:euclid-frame:0.22.3")
+   api("us.ihmc:euclid:0.22.4")
+   api("us.ihmc:euclid-frame:0.22.4")
    api("us.ihmc:ihmc-yovariables:0.13.6")
 }
 
