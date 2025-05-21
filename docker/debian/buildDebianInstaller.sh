b8f6805d0af90797f8fb9f14becc1f051a0ba50d
Duncan Calvert <dcalvert@ihmc.org>
2025-05-21 16:21:42 -0500

    Add docker image for building the debian installer. (#212)

---
+++ docker/debian/buildDebianInstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker build --tag ihmcrobotics/scs2-debian:0.1 .
+
+docker run \
+    --rm \
+    --network host \
+    --dns=1.1.1.1 \
+    --volume $(pwd)/../..:/simulation-construction-set-2 \
+    ihmcrobotics/scs2-debian:0.1 gradle buildDebianPackage
