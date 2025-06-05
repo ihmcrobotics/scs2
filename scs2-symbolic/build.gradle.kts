544b1c7ec4b5ac9b6376f1a2c6fe5fe356cf1ac9
Beomyeong <bpark@ihmc.org>
2025-06-05 13:52:32 -0500

    :arrow_up: euclid 0.22.5

--- scs2-symbolic/build.gradle.kts
+++ scs2-symbolic/build.gradle.kts
@@ -14,7 +14,7 @@ mainDependencies {
    api("us.ihmc:scs2-shared-memory:source")
 
    api("us.ihmc:euclid:0.22.5")
-   api("us.ihmc:euclid-frame:0.22.4")
+   api("us.ihmc:euclid-frame:0.22.5")
    api("us.ihmc:ihmc-yovariables:0.13.6")
 }
 
