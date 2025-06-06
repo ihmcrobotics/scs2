00abab7583bcfa85cb0a0e2ada1828a8fa41bd1d
bpark <bpark@ihmc.org>
2025-06-05 19:05:29 -0500

    Feature/euclid version up 0.22.5 (#214)

--- scs2-session-visualizer-jfx/build.gradle.kts
+++ scs2-session-visualizer-jfx/build.gradle.kts
@@ -24,9 +24,9 @@ mainDependencies {
    api(ihmc.javaFXModule("fxml", javaFXVersion))
    api(ihmc.javaFXModule("swing", javaFXVersion))
 
-   api("us.ihmc:euclid:0.22.4")
-   api("us.ihmc:euclid-shape:0.22.4")
-   api("us.ihmc:euclid-frame:0.22.4")
+   api("us.ihmc:euclid:0.22.5")
+   api("us.ihmc:euclid-shape:0.22.5")
+   api("us.ihmc:euclid-frame:0.22.5")
    api("us.ihmc:ihmc-graphics-description:0.26.2")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:ihmc-javafx-extensions:17-0.2.2")
