566fd8d9af8569b4e6d33bb08bfa93a3ceed447c
Dexton Anderson <danderson@ihmc.org>
2025-06-20 16:56:42 -0500

    :arrow_up: ihmc-build 1.2.1

--- scs2-session-logger/settings.gradle.kts
+++ scs2-session-logger/settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
    plugins {
-      id("us.ihmc.ihmc-build") version "1.1.1"
+      id("us.ihmc.ihmc-build") version "1.2.1"
    }
 }
 
@@ -11,7 +11,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:1.1.1")
+      classpath("us.ihmc:ihmc-build:1.2.1")
    }
 }
 
