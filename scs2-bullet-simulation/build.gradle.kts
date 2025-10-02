c0d91307ae41058bb61798ae9542f77d8e781ce4
Dexton Anderson <danderson@ihmc.org>
2025-10-02 10:11:52 -0500

    Use official JavaCPP

--- scs2-bullet-simulation/build.gradle.kts
+++ scs2-bullet-simulation/build.gradle.kts
@@ -25,10 +25,10 @@ mainDependencies {
    api("us.ihmc:ihmc-yovariables:0.13.6")
    api("us.ihmc:mecano-yovariables:17-0.19.2")
 
-   val bulletVersion = "3.25-1.5.11-ihmc-2"
-   api("us.ihmc:bullet:$bulletVersion")
-   api("us.ihmc:bullet:$bulletVersion:linux-x86_64")
-   api("us.ihmc:bullet:$bulletVersion:windows-x86_64")
+   val bulletVersion = "3.25-1.5.11"
+   api("org.bytedeco:bullet:$bulletVersion")
+   api("org.bytedeco:bullet:$bulletVersion:linux-x86_64")
+   api("org.bytedeco:bullet:$bulletVersion:windows-x86_64")
 }
 
 debugDependencies {
