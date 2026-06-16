b092c44a9d5b182d32bb841d2caf2bd9f4a86695
nkitchel <nkitchel@ihmc.org>
2026-06-16 11:36:15 -0500

    Tuned the Mujoco physics engine to use a faster sim dt and reduce the Z drift some what with the faster sim dt

--- scs2-mujoco-simulation/src/main/java/us/ihmc/scs2/simulation/mujoco/physicsEngine/parameters/MujocoSimulationParameters.java
+++ scs2-mujoco-simulation/src/main/java/us/ihmc/scs2/simulation/mujoco/physicsEngine/parameters/MujocoSimulationParameters.java
@@ -7,9 +7,12 @@ package us.ihmc.scs2.simulation.mujoco.physicsEngine.parameters;
 public class MujocoSimulationParameters
 {
    private double timestep = 0.002;
-   private int solverIterations = 100;
+   private int solverIterations = 25;
    private int subSteps = 1;
-   private double contactSolrefTimeconst = 0.005;
+   private double contactSolrefTimeconst = 0.02;
+   private double contactSolimpDmin = 0.9;
+   private double contactSolimpDmax = 0.99;
+   private int noslipIterations = 5;
 
    public static MujocoSimulationParameters defaultMujocoSimulationParameters()
    {
@@ -56,11 +59,44 @@ public class MujocoSimulationParameters
       this.contactSolrefTimeconst = contactSolrefTimeconst;
    }
 
+   public double getContactSolimpDmin()
+   {
+      return contactSolimpDmin;
+   }
+
+   public void setContactSolimpDmin(double contactSolimpDmin)
+   {
+      this.contactSolimpDmin = contactSolimpDmin;
+   }
+
+   public double getContactSolimpDmax()
+   {
+      return contactSolimpDmax;
+   }
+
+   public void setContactSolimpDmax(double contactSolimpDmax)
+   {
+      this.contactSolimpDmax = contactSolimpDmax;
+   }
+
+   public int getNoslipIterations()
+   {
+      return noslipIterations;
+   }
+
+   public void setNoslipIterations(int noslipIterations)
+   {
+      this.noslipIterations = noslipIterations;
+   }
+
    public void set(MujocoSimulationParameters other)
    {
       this.timestep = other.timestep;
       this.solverIterations = other.solverIterations;
       this.subSteps = other.subSteps;
       this.contactSolrefTimeconst = other.contactSolrefTimeconst;
+      this.contactSolimpDmin = other.contactSolimpDmin;
+      this.contactSolimpDmax = other.contactSolimpDmax;
+      this.noslipIterations = other.noslipIterations;
    }
 }
