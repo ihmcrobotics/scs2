8964f7fdc8e05e52e07acc95a310c8268840d556
nkitchel <nkitchel@ihmc.org>
2026-07-26 10:05:02 -0500

    :arrow_up: javaFX 17.0.8 -> 17.0.20

--- scs2-session-visualizer/build.gradle.kts
+++ scs2-session-visualizer/build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 mainDependencies {
    api("us.ihmc:scs2-definition:source")
 
-   var javaFXVersion = "17.0.8"
+   var javaFXVersion = "17.0.20"
    api(ihmc.javaFXModule("base", javaFXVersion)) // This is for using the property data structure. Not sure if that's the best thing to do.
 }
 
