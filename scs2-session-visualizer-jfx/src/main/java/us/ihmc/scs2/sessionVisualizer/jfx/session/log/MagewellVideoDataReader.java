494870b1178263cfafe70566300d57dcda9e2b44
nkitchel <nkitchel@ihmc.org>
2026-09-18 18:15:26 -0500

    Cleanup video playback to be faster and more unified

--- scs2-session-visualizer-jfx/src/main/java/us/ihmc/scs2/sessionVisualizer/jfx/session/log/MagewellVideoDataReader.java
+++ scs2-session-visualizer-jfx/src/main/java/us/ihmc/scs2/sessionVisualizer/jfx/session/log/MagewellVideoDataReader.java
@@ -1,13 +1,9 @@
 package us.ihmc.scs2.sessionVisualizer.jfx.session.log;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.PixelFormat;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import logger_msgs.Camera;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.JavaFXFrameConverter;
+import us.ihmc.concurrent.ConcurrentCopier;
 import us.ihmc.scs2.session.log.MagewellScrubber;
 import us.ihmc.scs2.session.log.ProgressConsumer;
 
@@ -20,7 +16,7 @@ public class MagewellVideoDataReader implements VideoDataReader
    private static final int MAX_NON_VIDEO_FRAMES_TO_SKIP = 256;
 
    private final MagewellScrubber magewellScrubber;
-   private final FrameData frameData = new FrameData();
+   private final ConcurrentCopier<FrameData> imageBuffer = new ConcurrentCopier<>(FrameData::new);
 
    public MagewellVideoDataReader(Camera camera, File dataDirectory, boolean hasTimeBase) throws IOException
    {
@@ -44,60 +40,35 @@ public class MagewellVideoDataReader implements VideoDataReader
       // The underlying FFmpegFrameGrabber.grabFrame() returns the next packet from any stream,
       // so a multi-stream MP4 (video + audio + timecode) may yield non-image frames here.
       int skipped = 0;
-      while (nextFrame != null && !hasImageData(nextFrame) && skipped < MAX_NON_VIDEO_FRAMES_TO_SKIP)
+      while (nextFrame != null && !FrameImageConverter.hasImageData(nextFrame) && skipped < MAX_NON_VIDEO_FRAMES_TO_SKIP)
       {
          nextFrame = magewellScrubber.getMagewellDemuxer().getNextFrame();
          skipped++;
       }
 
       // This is a copy that can be shown in the video view to debug timestamp issues
-      {
-         FrameData copyForWriting = frameData;
-         copyForWriting.queryRobotTimestamp = queryRobotTimestamp;
-         copyForWriting.currentRobotTimestamp = magewellScrubber.getCurrentRobotTimestamp();
-         copyForWriting.currentVideoTimestamp = magewellScrubber.getCurrentVideoTimestamp();
-         copyForWriting.currentDemuxerTimestamp = magewellScrubber.getMagewellDemuxer().getCurrentPTS();
-      }
+      FrameData copyForWriting = imageBuffer.getCopyForWriting();
+      copyForWriting.queryRobotTimestamp = queryRobotTimestamp;
+      copyForWriting.currentRobotTimestamp = magewellScrubber.getCurrentRobotTimestamp();
+      copyForWriting.currentVideoTimestamp = magewellScrubber.getCurrentVideoTimestamp();
+      copyForWriting.currentDemuxerTimestamp = magewellScrubber.getMagewellDemuxer().getCurrentPTS();
+      copyForWriting.frame = FrameImageConverter.convertFrameToWritableImage(nextFrame, copyForWriting.frame);
 
-      frameData.frame = convertFrameToWritableImage(nextFrame);
-   }
-
-   private static boolean hasImageData(Frame frame)
-   {
-      return frame.image != null && frame.imageWidth > 0 && frame.imageHeight > 0;
+      imageBuffer.commit();
    }
 
    /**
-    * This class converts a {@link Frame} to a {@link WritableImage} in order to be displayed correctly in JavaFX.
+    * Converts a {@link Frame} to a newly allocated {@link WritableImage} in order to be displayed correctly in JavaFX.
+    * <p>
+    * Prefer {@link FrameImageConverter#convertFrameToWritableImage(Frame, WritableImage)} when converting repeatedly, as it can reuse an image.
+    * </p>
     *
     * @param frameToConvert is the next frame we want to visualize so we convert it to be compatible with JavaFX
     * @return {@link WritableImage}
     */
    public static WritableImage convertFrameToWritableImage(Frame frameToConvert)
    {
-      Image currentImage;
-
-      if (frameToConvert == null || !hasImageData(frameToConvert))
-      {
-         return null;
-      }
-
-      try (JavaFXFrameConverter frameConverter = new JavaFXFrameConverter())
-      {
-         currentImage = frameConverter.convert(frameToConvert);
-      }
-      int width = (int) currentImage.getWidth();
-      int height = (int) currentImage.getHeight();
-
-      WritableImage writableImage = new WritableImage(width, height);
-      PixelReader pixelReader = currentImage.getPixelReader();
-      PixelWriter pixelWriter = writableImage.getPixelWriter();
-
-      int[] pixels = new int[width * height];
-      pixelReader.getPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), pixels, 0, width);
-      pixelWriter.setPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), pixels, 0, width);
-
-      return writableImage;
+      return FrameImageConverter.convertFrameToWritableImage(frameToConvert, null);
    }
 
    public void cropVideo(File outputFile, File timestampFile, long startTimestamp, long endTimestamp, ProgressConsumer progressConsumer) throws IOException
@@ -117,7 +88,7 @@ public class MagewellVideoDataReader implements VideoDataReader
 
    public FrameData pollCurrentFrame()
    {
-      return frameData;
+      return imageBuffer.getCopyForReading();
    }
 
    public int getCurrentIndex()
